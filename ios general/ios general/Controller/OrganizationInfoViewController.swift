501a1903ef9fdcc5341be430468b7bb95c0b2206
locovna <locovna@gmail.com>
2017-05-24 17:10:53 +0300

    Connect new view controller written in swift to UI

---
+++ ios general/ios general/Controller/OrganizationInfoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OrganizationInfoViewController.swift
+//  ios general
+//
+//  Created by lokovna on 5/24/17.
+//  Copyright © 2017 Darina Locovna. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OrganizationInfoViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        NSLog("OrganizationInfoViewController is here")
+    }
+}
