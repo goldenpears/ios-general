9b22531e679613d156bd8d96b2b26dc367daa4b8
locovna <locovna@gmail.com>
2017-05-25 13:18:55 +0300

    Show sum result in alert

--- ios general/ios general/Controller/OrganizationInfoViewController.swift
+++ ios general/ios general/Controller/OrganizationInfoViewController.swift
@@ -1,25 +1,19 @@
-//
-//  OrganizationInfoViewController.swift
-//  ios general
-//
-//  Created by lokovna on 5/24/17.
-//  Copyright © 2017 Darina Locovna. All rights reserved.
-//
-
 import Foundation
 import UIKit
 
 class OrganizationInfoViewController: UIViewController
 {
+    public var currentOrganization: OrganizationMO!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        NSLog("OrganizationInfoViewController is here")
     }
     
-    @IBAction func calculateSlaryButtonTapped(_ sender: UIButton)
+    @IBAction func calculateSalaryButtonTapped(_ sender: UIButton)
     {
-        let alert = UIAlertController(title: "Skin Type", message: "Please Choose Skin Type", preferredStyle: .alert)
+        let sum = currentOrganization.employees!.reduce(0) {$0 + $1.salary}
+        let alert = UIAlertController(title: "Calculate Salary", message: "Overall salary is: \(sum)", preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
         alert.addAction(cancelAction)
         self.present(alert, animated: true, completion: nil)
