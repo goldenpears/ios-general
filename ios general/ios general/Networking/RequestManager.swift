73ced54ffe7c73bedf077ceff187733d856a046b
locovna <locovna@gmail.com>
2017-05-26 19:25:28 +0300

    Setup Alamofire

--- ios general/ios general/Networking/RequestManager.swift
+++ ios general/ios general/Networking/RequestManager.swift
@@ -7,11 +7,25 @@
 //
 
 import Foundation
+import Alamofire
 
 class RequestManager: NSObject
 {
     static func fetchOrganizations(_: [String : AnyObject])
     {
         NSLog("Fetching!")
+        Alamofire.request("http://gitlab.faifly.com/ios-general/ios-tutorial/uploads/ffffec34fa4a727922bb1e09cc24b21d/tutorial.json").responseJSON
+        {
+            response in
+            print(response.request)  // original URL request
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value
+            {
+                print("JSON: \(JSON)")
+            }
+        }
     }
 }
